e6309e2609777778ecce974cb9c0a68be1a76580
AAJMvanMontfort <lout@me.com>
2020-07-05 12:28:20 +0200

    Final version

--- GestureRecognisers/ViewController.swift
+++ GestureRecognisers/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var lblImage: UILabel!
+    
+    var is2256 = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecogniser = UITapGestureRecognizer(target: self, action: #selector(changePicture))
+        
+        imageView.addGestureRecognizer(gestureRecogniser)
     }
 
-
+    @objc func changePicture() {
+        
+        if is2256 {
+            imageView.image = UIImage(named: "BR96 2288")
+            lblImage.text = "BR96 2288"
+            
+            is2256 = false
+        } else {
+            imageView.image = UIImage(named: "BR96 2256")
+            lblImage.text = "BR96 2256"
+            
+            is2256 = true
+        }
+    }
+    
 }
 
